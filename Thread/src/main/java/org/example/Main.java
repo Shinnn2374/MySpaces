6aa281d506ee4f7c7f5a12f90313c22bc738a4f2
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-03 21:15:10 +0300

    ...

--- Thread/src/main/java/org/example/Main.java
+++ Thread/src/main/java/org/example/Main.java
@@ -1,20 +1,44 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Начало выполнения программы");
-        Thread newThread = new Thread(new MyThread());
-        System.out.println("Запуск дочернего потока");
-        newThread.start();
+    public static void main(String[] args) throws InterruptedException  {
+// First task
+//        System.out.println("Начало выполнения программы");
+//        Thread newThread = new Thread(new MyThread());
+//        System.out.println("Запуск дочернего потока");
+//        newThread.start();
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Главный поток");
+//            try {
+//                Thread.sleep(1200);
+//            }catch (InterruptedException e){
+//                System.out.println("Прерывание главного потока");
+//            }
+//        }
+//        System.out.println("Программа выполнена");
+//   _______________________________________________
+// second task
+            Thread t = new Thread(new Runnable(){
+                public void run(){
+                    for (int i = 0; i < 5; i++) {
+                        System.out.println("Дочерний поток");
+                        try {
+                            Thread.sleep(1200);
+                        }catch (InterruptedException e){
+                            System.out.println("Прерывание дочернего потока");
+                        }
+                    }
+                }
+            });
+        System.out.println("Запускается дочерний поток");
+        t.start();
         for (int i = 0; i < 5; i++) {
             System.out.println("Главный поток");
-            try {
-                Thread.sleep(1200);
-            }catch (InterruptedException e){
-                System.out.println("Прерывание главного потока");
-            }
+            Thread.sleep(1200);
         }
-        System.out.println("Программа выполнена");
+        if (t.isAlive()) {
+            t.join();
+        }
+        System.out.println("Выполнение программы завершено");
     }
 }
