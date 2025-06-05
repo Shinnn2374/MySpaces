1a7de37d5578ff985a5eaacfbe816e7bb6a993e5
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-06-05 16:02:19 +0300

    ...

--- GUIExample/src/main/java/org/example/Main.java
+++ GUIExample/src/main/java/org/example/Main.java
@@ -1,12 +1,18 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main{
     public static void main(String[] args){
         JFrame frame = new JFrame();
         frame.setSize(600, 400);
+
+        frame.setLayout( new FlowLayout());
+        frame.add(new JButton("Click Me"));
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
